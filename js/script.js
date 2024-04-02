f457e8dc99614d1f7e5cb5fe08ef701d7d067bab
isacarlos <165579487+isacarlos@users.noreply.github.com>
2024-04-02 00:08:57 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    var sliderPrincipal = document.querySelector('.slider-principal');
+    sliderPrincipal.classList.add('active');
+});
+
+
+HTML SCSS JSResult Skip Results Iframe
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
